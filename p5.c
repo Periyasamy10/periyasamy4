98e842d1727f0d2614e15cf51332831602f35036
Periyasamy10 <35652731+Periyasamy10@users.noreply.github.com>
2018-01-30 16:45:51 +0530

    Create p5.c

---
+++ p5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+x,y=map(int,(input().split(' ')))
+for num in range(x,y + 1):
+
+      order = len(str(num))
+      sum = 0
+      temp = num
+      while temp > 0:
+       digit = temp % 10
+       sum += digit ** order
+       temp //= 10
+
+      if num == sum:
+       print(num)
+       }
